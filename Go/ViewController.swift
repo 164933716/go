23e55532b0758321839e741a80eab68b762fd9d7
Leshaokun <164933716@qq.com>
2021-03-13 15:10:12 +0800

    Initial Commit

---
+++ Go/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Go
+//
+//  Created by 乐绍坤 on .
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
